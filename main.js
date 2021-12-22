5274a219bff6f124bf6f46d0cf5bb4dcb46b2583
Dexx Stexx <drewpad@icloud.com>
2021-12-22 12:39:48 -0800

    Add multiple enemies

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ class gameItem {
         this.b = b;
     }
     collision(object1) {
-        if((this.x >= object1.x && this.x <= object1.x+object1.width) || (object1.x >= this.x && object1.x <= this.x+this.width)) {
-            if((this.y >= object1.y && this.y <= object1.y+object1.height) || (object1.y >= this.y && object1.y <= this.y+this.height)) {
+        if ((this.x >= object1.x && this.x <= object1.x + object1.width) || (object1.x >= this.x && object1.x <= this.x + this.width)) {
+            if ((this.y >= object1.y && this.y <= object1.y + object1.height) || (object1.y >= this.y && object1.y <= this.y + this.height)) {
                 return true;
             } else {
                 return false;
@@ -20,7 +20,7 @@ class gameItem {
         }
     }
     drawItem() {
-        mainContext.fillStyle = "rgb("+this.r.toString()+", "+this.g.toString()+", "+this.b.toString()+")";
+        mainContext.fillStyle = "rgb(" + this.r.toString() + ", " + this.g.toString() + ", " + this.b.toString() + ")";
         mainContext.fillRect(this.x, this.y, this.width, this.height);
     }
 }
@@ -43,18 +43,20 @@ var mainContext = mainCanvas.getContext("2d");
 var player = new gameItem(100, 100, 0, 0, 255, 0, 0);
 player.drawItem();
 
-var badGuy = new gameItem(100, 100, document.body.clientWidth-100, 0, 0, 255, 255);
-badGuy.drawItem();
+var badGuys = [];
+for(var i = 0; i < Math.floor(document.body.clientWidth/200); i ++) {
+    badGuys.push(new gameItem(50, 50, document.body.clientWidth + 200*(i+1), Math.floor(Math.random() * (document.body.clientHeight - 100)), 0, 255, 255));
+}
 
 function updateCanvasSize() {
-    mainCanvas.style.width = document.body.clientWidth.toString()+"px";
-    mainCanvas.style.height = document.body.clientHeight.toString()+"px";
+    mainCanvas.style.width = document.body.clientWidth.toString() + "px";
+    mainCanvas.style.height = document.body.clientHeight.toString() + "px";
     mainCanvas.width = document.body.clientWidth;
     mainCanvas.height = document.body.clientHeight;
 }
 
 function getInput(e) {
-    if(e.which === 38) {
+    if (e.which === 38) {
         keybuffer = "up";
     } else if (e.which === 40) {
         keybuffer = "down";
@@ -69,16 +71,16 @@ function resetInput() {
 }
 
 function test(timestamp) {
-    if(start === undefined) {
+    if (start === undefined) {
         start = timestamp;
     }
     mainContext.fillStyle = '#FFFFFF';
     mainContext.fillRect(0, 0, document.body.clientWidth, document.body.clientHeight);
     player.drawItem();
     mainContext.font = "50px sans-serif"
-    if(keybuffer === "down") {
+    if (keybuffer === "down") {
         player.y += 10;
-    } else if(keybuffer === "up") {
+    } else if (keybuffer === "up") {
         player.y -= 10;
     }
     if (player.y + player.height > document.body.clientHeight) {
@@ -86,25 +88,27 @@ function test(timestamp) {
     } else if (player.y < 0) {
         player.y = 0;
     }
-    if (player.collision(badGuy) === true) {
-        badGuy.x = document.body.clientWidth ;
-        badGuy.y = Math.floor(Math.random()*(document.body.clientHeight-100));
-        lives--;
-    }
-    badGuy.drawItem();
-    badGuy.x -= 10 + (score * 0.005);
-    if(badGuy.x < -100) {
-        badGuy.x = document.body.clientWidth;
-        badGuy.y = Math.floor(Math.random()*(document.body.clientHeight-100));
-        score += 100;
+    for (var i = 0; i < badGuys.length; i++) {
+        if (player.collision(badGuys[i]) === true) {
+            badGuys[i].x = document.body.clientWidth;
+            badGuys[i].y = Math.floor(Math.random() * (document.body.clientHeight - 100));
+            lives--;
+        }
+        badGuys[i].drawItem();
+        badGuys[i].x -= 10 + (score * 0.001);
+        if (badGuys[i].x < -100) {
+            badGuys[i].x = document.body.clientWidth;
+            badGuys[i].y = Math.floor(Math.random() * (document.body.clientHeight - 100));
+            score += 20;
+        }
     }
     mainContext.fillStyle = "#000000";
-    mainContext.fillText("Lives: "+lives.toString(), document.body.clientWidth-200, 50);
-    mainContext.fillText("Score: "+score.toString(), 10, 50);
-    if(lives !== 0) {
+    mainContext.fillText("Lives: " + lives.toString(), document.body.clientWidth - 200, 50);
+    mainContext.fillText("Score: " + score.toString(), 10, 50);
+    if (lives !== 0) {
         window.requestAnimationFrame(test);
     } else {
-        mainContext.fillText("Game Over", 0.5*document.body.clientWidth, 0.5*document.body.clientHeight);
+        mainContext.fillText("Game Over", 0.5 * document.body.clientWidth, 0.5 * document.body.clientHeight);
     }
 }
 
